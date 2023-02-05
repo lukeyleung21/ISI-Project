f3d19d0370ebbc22f7c9046d5e11e9c5b94f296f
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2023-02-06 01:10:55 +0800

    Shop temp grid

--- isi/src/views/shop.vue
+++ isi/src/views/shop.vue
@@ -1,7 +1,42 @@
 <template>
-<p>shop</p>
+<v-container fluid>
+<v-row>
+<v-col lg="8">
+<h1>Electric Appliance Shop🛒</h1>
+</v-col>
+<v-col>
+<v-text-field
+lg="6"
+solo
+label="Search Here🔍"
+v-model="search"
+></v-text-field>
+</v-col>
+</v-row>
+<v-divider></v-divider>
+Developing Here
+<v-divider></v-divider>
+<v-row>
+<v-card v-for="item in items" max-width="400" class="mx-16 my-15">
+<v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+    <v-card-title>{{ item.name }}</v-card-title>
+    Information Here....
+</v-card>
+</v-row>
+</v-container>
 </template>
 
 <script>
-
+export default{
+  data(){
+    return{
+      search : '',
+      items:[{name:'test1',price:'50'},{name:'test2',price:'50'},{name:'test3',price:'50'},{name:'test4',price:'50'},{name:'test5',price:'50'}
+  ]
+    }
+  }
+}
 </script>
