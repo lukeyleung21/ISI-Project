7c065f535acbcc6e079b15fe26c1b708e658c764
Steve Lam <sayounarailoveyou@gmail.com>
2023-02-12 02:23:40 +0900

    modified:   isi/src/views/purchaseOrder.vue

--- isi/src/views/purchaseOrder.vue
+++ isi/src/views/purchaseOrder.vue
@@ -1,5 +1,31 @@
 <template>
-    <p>Purchase Order</p>
+    <v-simple-table fixed-header height="300px">
+        <template v-slot:default>
+        <thead>
+        <tr>
+        <th class="text-left">State</th>
+        <th class="text-left">Full Name</th>
+        <th class="text-left">Email</th>
+        <th class="text-left">Address</th>
+        <th class="text-left">Quantity</th>
+        <th class="text-left">Total Amount</th>
+        <th class="text-left">Date</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr v-for="userdata in userdata" :key="userdata">
+            <td>{{ userdata.fName }}</td>
+            <td>{{ userdata.email }}</td>
+            <td>{{ userdata.address }}</td>
+            <td>{{ userdata.fName }}</td>
+            <td>{{ userdata.email }}</td>
+            <td>{{ userdata.address }}</td>
+            <td>{{ userdata.address }}</td>
+            </tr>
+       
+        </tbody>
+        </template>
+    </v-simple-table>
 </template>
 
 <script>
@@ -20,6 +46,23 @@ export default {
                 router.push("/login");
             } 
         },
+
+        async orderlist () {
+            fetch('http://localhost:8000/order', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({fName: this.fName, email: this.email, password: this.password, address: this.address})
+            })
+            .then((response) => response.json())
+            .then((data) => {
+
+            });
+        },
+
+        async created(){
+        await this.loadData();
+        },
+       
     },
     
 }
